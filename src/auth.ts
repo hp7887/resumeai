095e29ecbc66aa4a89f7e5c21030284b8c588aa6
discordby <dev@discord.by>
2025-07-19 01:39:15 +0300

    refactor(auth): remove test comment from auth module

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,6 @@ import NextAuth from 'next-auth'
 import { withPayload } from 'payload-authjs'
 import { authConfig } from './auth.config'
 
-#test
 export const { handlers, signIn, signOut, auth } = NextAuth(
   withPayload(authConfig, {
     payloadConfig,

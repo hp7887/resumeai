b698c1fa511f7e619249b27d96a4612a2a3c897d
Bluremind <86150771+Bluremind@users.noreply.github.com>
2025-07-18 19:32:05 +0300

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
-// src/auth.ts
 import payloadConfig from '@payload-config'
 import NextAuth from 'next-auth'
 import { withPayload } from 'payload-authjs'
 import { authConfig } from './auth.config'
 
+#test
 export const { handlers, signIn, signOut, auth } = NextAuth(
   withPayload(authConfig, {
     payloadConfig,
